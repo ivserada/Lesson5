a79ab7568ac6906f3ab6e7e4ee039bc4502e2ae4
ivserada <ivserada@gmail.com>
2026-09-08 22:09:48 +0300

    feat: task4 resolution

--- task1.ts
+++ task1.ts
@@ -1,1 +1,12 @@
 // Написать функцию, которая будет высчитывать сумму чисел от нуля, до параметра, который мы в неё передаем.
+console.log(sumNumbers(5.5));
+function sumNumbers(n: number): number {
+  let result: number = 0;
+  for (let i = 0; i <= n; i++) {
+    result += i;
+  }
+  if (!Number.isInteger(n)) {
+    result += n - Math.floor(n);
+  }
+  return result;
+}
--- task4.ts
+++ task4.ts
@@ -2,3 +2,27 @@
 // Пароль должен содержать как минимум одну заглавную букву и цифру
 // Пароль только из латинских букв
 // Длина пароля должна быть не менее 10 символов
+function validatePassword(password: string): boolean {
+  // 1. Длина не менее 10 символов
+  if (password.length < 10) {
+    return false;
+  }
+
+  const latinOnly = /^[A-Za-z0-9]+$/; // 2. Только латинские буквы и цифры
+  if (!latinOnly.test(password)) {
+    return false;
+  }
+
+  const hasUpperCase = /[A-Z]/.test(password); // 3. Как минимум одна заглавная буква
+  if (!hasUpperCase) {
+    return false;
+  }
+
+  const hasDigit = /[0-9]/.test(password); // 4. Как минимум одна цифра
+  if (!hasDigit) {
+    return false;
+  }
+
+  return true;
+}
+console.log(validatePassword("123ASDaaafвввв"));
